0aeb121384311b049146846717d22f807965eadb
Aitor Gallardo <38296940+AitorGallardo@users.noreply.github.com>
2023-06-17 23:36:02 +0200

    list de results, hidden by now

--- TODO.md
+++ TODO.md
@@ -5,11 +5,10 @@
 [X] Filter data with selectors: by crew, by name
 [X] Filter by power un range component
 [X] Sorting by name, range power (asc/desc)
-[ ] Meter acordeon al fitler con nombre negrita y que puedas clickar sugerencia para que salga esa
-[ ] Meter debouncer al input.
-
+<!-- [ ] Meter debouncer al input. -->
 [ ] mobile version
 [ ] improve and clear, like fetch better
 
 [ ] cambiar modo grid a modo acordeon
 [ ] mejorar fotos
+[ ] Meter acordeon al fitler con nombre negrita y que puedas clickar sugerencia para que salga esa. Poner abajo un boton con +3 results y q se expanda, quando solo queda uno quita toods, meter el subrayado al busqcar del nomber?quitar busqueda por tripulacion,
--- src/components/search_bar/SearchBar.jsx
+++ src/components/search_bar/SearchBar.jsx
@@ -1,30 +1,44 @@
-import { useState } from "react"
+import { useState } from 'react';
 import searchIcon from '../../assets/search-icon.svg';
 import PropTypes from 'prop-types';
 
-import './SearchBar.css'
+import './SearchBar.css';
 
-export const SearchBar = ({handleOnChangeValue}) => {
+export const SearchBar = ({ handleOnChangeValue }) => {
+  const [value, setValue] = useState('');
 
-    const [value, setValue] = useState('')
-
-    const handleOnChange = ({target}) =>{
-        const val = target.value;
-        setValue(val)
-        handleOnChangeValue({name:'word', value:val})
-    }
+  const handleOnChange = ({ target }) => {
+    const val = target.value;
+    setValue(val);
+    handleOnChangeValue({ name: 'word', value: val });
+  };
 
   return (
-    <div className="searchBar__container">
-     <input className="searchBar__input" type="text" placeholder="Search" value={value} onChange={handleOnChange}/>   
-     <button className="searchBar__button">
-     <img className="searchBar__search-icon" src={searchIcon} alt="Your SVG" />
-    </button>
+    <div className='searchBar__container'>
+      <div className='searchBar__input-container'>
+        <input
+          className='searchBar__input'
+          type='text'
+          placeholder='Search'
+          value={value}
+          onChange={handleOnChange}
+        />
+        <button className='searchBar__button'>
+          <img
+            className='searchBar__search-icon'
+            src={searchIcon}
+            alt='Your SVG'
+          />
+        </button>
+      </div>
+      {/* <ul>
+        <li>hola</li>
+        <li>adios</li>
+      </ul> */}
     </div>
-  )
-}
+  );
+};
 
 SearchBar.propTypes = {
   handleOnChangeValue: PropTypes.func,
 };
-
